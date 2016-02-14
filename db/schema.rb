48c31f2540acd476b0763331a019fc974262ecde
kpjani <kpjani@ncsu.edu>
2016-02-14 19:47:11 +0530

    test cases on user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214024354) do
+ActiveRecord::Schema.define(version: 20160214133519) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -21,5 +21,7 @@ ActiveRecord::Schema.define(version: 20160214024354) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
