8effac417b35271c94ed75927935230c22f45ef9
kpjani <kpjani@ncsu.edu>
2016-02-19 00:03:42 +0530

    Validations added for new course form

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,13 @@ class Course < ActiveRecord::Base
   has_many :teaches
   has_many :instructors, through: :teaches
   has_many :cmaterials
+
+  validates :number, presence: true, length: { maximum: 50 },
+            uniqueness: { case_sensitive: false }
+
+  validates :title, presence: true, length: { maximum: 255 },
+            uniqueness: { case_sensitive: false }
+
+  validates :description, presence: true
+
 end
